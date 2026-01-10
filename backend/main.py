9818b1c50078dcefefb4f0bfe92cd5613c68f60f
MR-Crisp <aka.amin2005@gmail.com>
2026-01-10 23:06:06 +0000

    got basic fast api functionality to work

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,28 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
+from model import predict
 
 
 class SentimentRequest(BaseModel):
-    text: str
+    text: str = Field(...,description="Text for sentiment to be calculated on")
+
+class SentimentResponse(BaseModel):
+    text: str = Field(..., description="Text for sentiment to be calculated on")
+    sentiment_val: float = Field(..., description="Sentiment value, 0 for negative, 1 for positive")
+    sentiment: str = Field(..., description="The sentiment of the text")
 
 app = FastAPI()
-@app.get("/"):
+@app.get("/")
 def root():
     return {"message": "Hello World"}
-@app.get("/model"):
-def model(request: SentimentRequest):
-    return {"text": request.text}
+@app.post("/model", response_model=SentimentResponse)
+def calculateSentiment(request: SentimentRequest):
+    text = request.text
+    sentiment, confidence = predict(text)
+    return SentimentResponse(
+            text=request.text,
+            sentiment_val=confidence,
+            sentiment=sentiment
+
+        )
+
