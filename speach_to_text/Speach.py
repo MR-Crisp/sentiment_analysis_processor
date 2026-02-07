deea8488ed4342742ec5223db11ca8379b2b95fc
MR-Crisp <aka.amin2005@gmail.com>
2026-02-07 23:22:14 +0000

    created basic litening, and updated gitignore

--- speach_to_text/Speach.py
+++ speach_to_text/Speach.py
@@ -1,2 +1,10 @@
-def record_text():
-    pass
+import sounddevice as sd
+import soundfile as sf
+
+fs = 44100
+seconds = 5
+
+recording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+sd.wait()
+
+sf.write("output.wav", recording, fs)
