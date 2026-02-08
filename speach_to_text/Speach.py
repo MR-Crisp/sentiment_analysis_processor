75c364d9a5ecbc85b18c0f476cfb4e2bae15f6f9
MR-Crisp <aka.amin2005@gmail.com>
2026-02-08 17:31:41 +0000

    Added text to speach using already made model (Open AI) which will be changed in the future

--- speach_to_text/Speach.py
+++ speach_to_text/Speach.py
@@ -1,10 +1,22 @@
 import sounddevice as sd
 import soundfile as sf
 
-fs = 44100
-seconds = 5
+import whisper
 
-recording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
-sd.wait()
 
-sf.write("output.wav", recording, fs)
+def record_aduio(seconds):
+
+    fs = 44100
+    print("Recording...")
+    recording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+    sd.wait()
+
+    sf.write("output3.wav", recording, fs)
+    print("Done.")
+
+def transcribe_aduio(filename):
+    model = whisper.load_model("base")  # tiny, base, small, medium, large
+    result = model.transcribe(filename)
+
+    return result["text"]
+
